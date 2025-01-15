f2aad1324f849734f10897656d2a7eaf2a4e64b9
shiha <shiha@DESKTOP-RES15AU>
2025-01-15 20:05:46 +0600

    all push

---
+++ Object Repository/Page_Contact  Practice Test Automation  Sel_b632b1/div_If you have questions, suggestions, or _1fdbbf.rs
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_If you have questions, suggestions, or _1fdbbf</name>
+   <tag></tag>
+   <elementGuidId>13a9f3bc-501a-49e7-a9c9-668643aa5e72</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.post-content</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='loop-container']/div/article/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>fc7e2e49-227e-4a8c-a8a6-1b5f1ace46bd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>post-content</value>
+      <webElementGuid>bc5c2864-77e4-4c3d-b30a-18aacccece16</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+			
+
+
+
+
+If you have questions, suggestions, or business proposals, fill up the form below and I’ll be in touch as soon as I get it!
+
+
+Please enable JavaScript in your browser to complete this form.Name *FirstLastEmail *Please enter a valid email address.Comment or Message *MessageSubmit  
+
+
+ Thank you, Dmitry Shyshkin Happy Testing!
+					</value>
+      <webElementGuid>acbe8e2f-9e92-41d8-aafe-fe11d430b15f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;loop-container&quot;)/div[@class=&quot;post-15 page type-page status-publish hentry entry&quot;]/article[1]/div[@class=&quot;post-content&quot;]</value>
+      <webElementGuid>98e19c06-0b69-4ac0-b8f7-a6802925875b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='loop-container']/div/article/div[2]</value>
+      <webElementGuid>c36f2d91-2909-4aa6-b550-eb02790b73c0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Contact'])[2]/following::div[1]</value>
+      <webElementGuid>82031aa0-dd08-425a-bd26-257ad955ebb7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='open menu'])[1]/following::div[4]</value>
+      <webElementGuid>1ba6d68c-eb26-4b8a-9c31-33e3affa0c58</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//article/div[2]</value>
+      <webElementGuid>a83b4085-15c4-476b-aba5-eedd01a2c470</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+			
+
+
+
+
+If you have questions, suggestions, or business proposals, fill up the form below and I’ll be in touch as soon as I get it!
+
+
+Please enable JavaScript in your browser to complete this form.Name *FirstLastEmail *Please enter a valid email address.Comment or Message *MessageSubmit  
+
+
+ Thank you, Dmitry Shyshkin Happy Testing!
+					' or . = '
+			
+
+
+
+
+If you have questions, suggestions, or business proposals, fill up the form below and I’ll be in touch as soon as I get it!
+
+
+Please enable JavaScript in your browser to complete this form.Name *FirstLastEmail *Please enter a valid email address.Comment or Message *MessageSubmit  
+
+
+ Thank you, Dmitry Shyshkin Happy Testing!
+					')]</value>
+      <webElementGuid>33745fdd-246e-4c23-9731-6f5d29f7e22a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
