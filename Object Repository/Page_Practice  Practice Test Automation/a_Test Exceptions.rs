309d5c817061d4a0a38a021ab3677d921bc92cc1
shiha <shiha@DESKTOP-RES15AU>
2025-01-15 19:39:30 +0600

    ffdfd

---
+++ Object Repository/Page_Practice  Practice Test Automation/a_Test Exceptions.rs
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Test Exceptions</name>
+   <tag></tag>
+   <elementGuidId>315178e4-298c-452a-a091-3f1dfac30601</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.wp-block-columns.is-layout-flex.wp-container-core-columns-is-layout-2.wp-block-columns-is-layout-flex > div.wp-block-column.is-layout-flow.wp-block-column-is-layout-flow > p > a</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='loop-container']/div/article/div[2]/div[2]/div/p/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>c5becf8d-7b6c-4c78-8c06-82cb3b9addca</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://practicetestautomation.com/practice-test-exceptions/</value>
+      <webElementGuid>94369397-a1c6-4651-be5f-160f76b7970e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Test Exceptions</value>
+      <webElementGuid>a465e605-e782-478d-8ef8-0a981b7bcde6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;loop-container&quot;)/div[@class=&quot;post-11 page type-page status-publish hentry entry&quot;]/article[1]/div[@class=&quot;post-content&quot;]/div[@class=&quot;wp-block-columns is-layout-flex wp-container-core-columns-is-layout-2 wp-block-columns-is-layout-flex&quot;]/div[@class=&quot;wp-block-column is-layout-flow wp-block-column-is-layout-flow&quot;]/p[1]/a[1]</value>
+      <webElementGuid>2916ea17-77da-4ecc-8f8e-27d1e1085309</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='loop-container']/div/article/div[2]/div[2]/div/p/a</value>
+      <webElementGuid>a823e8dd-155f-4331-8379-657c1467527f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Test Exceptions')]</value>
+      <webElementGuid>52994912-0eda-4534-b105-8a95a0ee0fa4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Simple login page where testers can execute positive and negative login test cases'])[1]/following::a[1]</value>
+      <webElementGuid>58ddcc0e-7ccc-4b2a-9f24-9dd5c9266c91</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Test Login Page'])[1]/following::a[1]</value>
+      <webElementGuid>d200807a-e8d3-4405-be75-56078563c6de</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Page to reproduce the most common Selenium Exceptions.'])[1]/preceding::a[1]</value>
+      <webElementGuid>c74abaa4-db76-4f33-a9ab-4969295c0854</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Test Exceptions']/parent::*</value>
+      <webElementGuid>ae29a73c-5f09-4a23-8f62-42675f02a107</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://practicetestautomation.com/practice-test-exceptions/')]</value>
+      <webElementGuid>dfbbc02f-1d0d-43f2-8def-005058f22c65</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/p/a</value>
+      <webElementGuid>419d6ff1-025a-461e-a7de-f665b4031f28</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://practicetestautomation.com/practice-test-exceptions/' and (text() = 'Test Exceptions' or . = 'Test Exceptions')]</value>
+      <webElementGuid>09a918ad-a9ea-47bd-b202-fbec671e2cd4</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
